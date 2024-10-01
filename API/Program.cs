2d85e9860b3247717a095544e81894625e9db7f4
Joao B. <93290335+joaocba@users.noreply.github.com>
2024-10-01 23:36:35 +0100

    Security Headers

--- API/Program.cs
+++ API/Program.cs
@@ -27,13 +27,42 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
+// SECURITY IMPLEMENTATION (NuGet NWebsec lib)
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+
+// Use X-Content-Type-Options header for security improvement of the  application
+app.UseXContentTypeOptions();
+
+// Use of the Referrer-Policy header for security improvement of the application
+app.UseReferrerPolicy(opt => opt.NoReferrer()); //  No Referrer Policy will return less info  to the attacker
+
+// Use XXss Protection against  XSS attacks
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+
+// Main Defense against CSRF attacks and XSRF attacks
+app.UseCspReportOnly(opt => opt
+   .BlockAllMixedContent()
+   .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://fonts.gstatic.com").UnsafeInline()) // Allow inline styles temporarily
+   .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+   .FormActions(s => s.Self())
+   .FrameAncestors(s => s.Self())
+   .ImageSources(s => s.Self().CustomSources("blob:", "https://res.cloudinary.com"))
+   .ScriptSources(s => s.Self().UnsafeInline())  // Temporarily allow unsafe inline scripts
+);
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 // Use CORS policy before Authorization
